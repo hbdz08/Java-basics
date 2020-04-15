c1359015382b1680deee1a57bc1cc0f8408ba83f
lau <hbdz08@163.com>
2020-04-15 14:26:58 +0800

    first push

---
+++ src/test/java/com/lau/javabasics/JavabasicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lau.javabasics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavabasicsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
